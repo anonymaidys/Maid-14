3ffd0162d3c0f892371d698c28605f441cc7d112
RedBurningPhoenix <Warete4@gmail.com>
2026-01-13 22:00:45 +0500

    double fix

--- Content.Shared/_White/CVars.cs
+++ Content.Shared/_White/CVars.cs
@@ -5,8 +5,8 @@ namespace Content.Shared._White;
 [CVarDefs]
 public sealed class WhiteCVars
 {
-    public static readonly CVarDef<double> GhostRespawnTime =
-        CVarDef.Create("ghost.respawn_time", 15d, CVar.SERVERONLY);
+    public static readonly CVarDef<float> GhostRespawnTime =
+        CVarDef.Create("ghost.respawn_time", 15f, CVar.SERVERONLY);
 
     public static readonly CVarDef<int> GhostRespawnMaxPlayers =
         CVarDef.Create("ghost.respawn_max_players", 40, CVar.SERVERONLY);
